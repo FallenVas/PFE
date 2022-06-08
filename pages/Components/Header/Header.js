d1fcb1f80df591b99cdbfecae3a2b0a309a9b394
FallenVas <elouahdizaki@gmail.com>
2022-06-08 01:48:07 +0100

    current Progress

---
+++ pages/Components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+import Logo from './Logo.svg'
+const styles = {
+    navLink : 'hover:gradientPrimary cursor-pointer font-semibold'
+}
+
+const Header = () => {
+  return (
+    <div className=' py-5 px-20 flex justify-between items-center border-b border-solid border-black'>
+        <div className='flex gap-2'>
+            <Image width={50} height={50} src={Logo} />
+            <h2 className='gradientPrimary text-4xl font-bold'>CryptoSwap</h2>
+        </div>
+        <div className='flex gap-10'>
+             <a className={styles.navLink}>Home</a>   
+             <a className={styles.navLink}>AboutUs</a>   
+             <a className={styles.navLink}>Services</a>   
+             <a className={styles.navLink}>ContactUs</a>   
+        </div>
+        <div>
+            <button className='py-2 px-6 text-center gradientBackground rounded'>Login In</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        Black:'#222222'
+      }
+    },
   },
   plugins: [],
 }
